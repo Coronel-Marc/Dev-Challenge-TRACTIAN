a30db0c04833b519ee8c2a6bbf3bd994ebfff15c
Marcos Gabriel <56852736+Coronel-Marc@users.noreply.github.com>
2021-02-20 16:44:29 -0300

    Inicio do CRUD

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-console.log('Que a força esteja conosco!')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+app.set('view engine','ejs')
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
+app.listen(3000, function(){
+    console.log('Server running on port 3000')
+})
+
+app.get('/', (req,res)=>{
+    res.render('index.ejs')
+})
+
+app.post('/show', (req, res)=>{
+    console.log(req.body)
+})
